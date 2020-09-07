789efbd0457156f94e7fde856de3685c90d64344
Marianna <vergunmari@gmail.com>
2020-09-08 00:22:09 +0300

    feat: add base layout for detail view

--- client/src/components/Schedule/ScheduleDetailView/index.tsx
+++ client/src/components/Schedule/ScheduleDetailView/index.tsx
@@ -1,13 +1,26 @@
+import { Spin } from 'antd';
+import { API_Events, Event } from 'services/event';
 import viewStyles from './ScheduleDetailView.module.scss';
 
 interface IScheduleDetailView {
+  eventId: Event['id'];
   classes?: Array<string>;
 }
 
 export function ScheduleDetailView(props: IScheduleDetailView) {
+  const { eventId } = props,
+    { eventData, eventLoading } = API_Events.hooks.useEventData({ eventId });
+
   return (
     <section className={[viewStyles.ScheduleDetailView, props.classes].filter(Boolean).join(' ')}>
-      <h2>Detail View</h2>
+      {eventLoading === false ? (
+        <>
+          <h2>Detail View</h2>
+          {console.log('@@', eventData)}
+        </>
+      ) : (
+        <Spin wrapperClassName={viewStyles.spinner} />
+      )}
     </section>
   );
 }
