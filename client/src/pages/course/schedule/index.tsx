57f269235e7577869ab554f2972acab929b11f9e
Marianna <vergunmari@gmail.com>
2020-09-09 14:21:46 +0300

    fix: 'useCallback' is declared but its value is never read

--- client/src/pages/course/schedule/index.tsx
+++ client/src/pages/course/schedule/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { Row, Select, Switch } from 'antd';
 import { PageLayout } from 'components';
 import { useState } from 'react';
