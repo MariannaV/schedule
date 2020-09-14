0882c1733480b3897fbb22612bea50f67b722e8f
Pavel Bobryshev <bobr.8590@gmail.com>
2020-09-14 15:12:56 +0300

    calendar api try

--- client/src/components/Schedule/ScheduleCalendar.tsx
+++ client/src/components/Schedule/ScheduleCalendar.tsx
@@ -1,32 +1,48 @@
-import { Calendar, Badge } from 'antd';
+import { Calendar, Badge, Spin } from 'antd';
+import { API_Events } from 'services/event';
 
 export function ScheduleCalendar() {
+  const { eventsData, eventsLoading } = API_Events.hooks.useEventsData();
+  if (eventsLoading) return <Spin />;
+  const days = [] as any;
+  const months = [] as any;
+  eventsData.forEach((elem) => {
+    const transformed = new Date(elem.dateTime).getDay() + 1;
+    days.push(transformed);
+  });
+  eventsData.forEach((elem) => {
+    const transformed = new Date(elem.dateTime).getMonth() + 1;
+    months.push(transformed);
+  });
+
+  console.log(eventsData);
+
   function getListData(value) {
     let listData;
     switch (value.date()) {
-      case 8:
+      case days[0] && months[0]:
         listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
+          { type: 'warning', content: `${eventsData[0].name}` },
+          { type: 'success', content: `${eventsData[0].name}` },
         ];
         break;
-      case 10:
-        listData = [
-          { type: 'warning', content: 'This is warning event.' },
-          { type: 'success', content: 'This is usual event.' },
-          { type: 'error', content: 'This is error event.' },
-        ];
-        break;
-      case 15:
-        listData = [
-          { type: 'warning', content: 'This is warning event' },
-          { type: 'success', content: 'This is very long usual event。。....' },
-          { type: 'error', content: 'This is error event 1.' },
-          { type: 'error', content: 'This is error event 2.' },
-          { type: 'error', content: 'This is error event 3.' },
-          { type: 'error', content: 'This is error event 4.' },
-        ];
-        break;
+      // case 10:
+      //   listData = [
+      //     { type: 'warning', content: 'This is warning event.' },
+      //     { type: 'success', content: 'This is usual event.' },
+      //     { type: 'error', content: 'This is error event.' },
+      //   ];
+      //   break;
+      // case 15:
+      //   listData = [
+      //     { type: 'warning', content: 'This is warning event' },
+      //     { type: 'success', content: 'This is very long usual event。。....' },
+      //     { type: 'error', content: 'This is error event 1.' },
+      //     { type: 'error', content: 'This is error event 2.' },
+      //     { type: 'error', content: 'This is error event 3.' },
+      //     { type: 'error', content: 'This is error event 4.' },
+      //   ];
+      //   break;
       default:
     }
     return listData || [];
@@ -60,5 +76,13 @@ export function ScheduleCalendar() {
       </div>
     ) : null;
   }
-  return <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />;
+  return (
+    <section>
+      {eventsLoading === false ? (
+        <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
+      ) : (
+        <Spin spinning={Boolean(eventsLoading)} />
+      )}
+    </section>
+  );
 }
