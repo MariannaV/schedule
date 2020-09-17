a60933accf646b7e9c5b3f74547bbed1791cb4f8
Andrzej Puzyński <andrzej@Andrzejs-MacBook-Pro.local>
2020-09-17 22:13:15 +0200

    fix: Adding return if input doesn't exist

--- client/src/pages/course/schedule/index.tsx
+++ client/src/pages/course/schedule/index.tsx
@@ -16,20 +16,20 @@ enum View {
   calendar = 'Calendar',
 }
 
-const converttoPDF = () => {
+function converttoPDF() {
   const myinput = document.getElementById(`__next`);
+  if (!myinput) return;
   html2canvas(myinput).then((canvas) => {
     const pdfWidth = window.innerWidth;
     const pdfHeight = myinput.offsetHeight;
     const imgData = canvas.toDataURL('image/jpeg', 0.5);
-
     const orientation = pdfWidth > pdfHeight ? 'l' : 'p';
     const pixelratio = 3.7 / window.devicePixelRatio;
     const pdf = new jsPDF(orientation, 'mm', [pdfWidth / pixelratio, pdfHeight / pixelratio]);
     pdf.addImage(imgData, 'JPG', 0, 0);
     pdf.save(`RSFile.pdf`);
   });
-};
+}
 
 function SchedulePage() {
   const [currentView, changeView] = React.useState<View>(View.table);
