19e904e76362b3fa57b4f8380ce47aa4ef1dfdd7
Sykpen <48993005+Sykpen@users.noreply.github.com>
2020-09-26 01:14:17 +0300

    feat: add map view for events (#22)
    * feat: add map dependencies
    
    * feat: add map view for events
    
    * feat: connect Map component to FieldView
    
    * feat: replace 'place' input to 'places' Map on detail view
    
    Co-authored-by: Marianna <vergunmari@gmail.com>

--- client/src/components/Forms/FormItem/index.tsx
+++ client/src/components/Forms/FormItem/index.tsx
@@ -3,10 +3,11 @@ import { Form, Checkbox, Switch, Tag } from 'antd';
 import moment from 'moment';
 import { FormItemProps } from 'antd/lib/form';
 import formItemStyles from './FormItem.module.scss';
+import { MapView } from 'components/Schedule/ScheduleMap/map';
 
 export interface IFormItem extends FormItemProps {
   isReadOnly?: boolean;
-  type: 'input' | 'select' | 'time' | 'checkbox' | 'switch' | 'files';
+  type: 'input' | 'select' | 'time' | 'checkbox' | 'switch' | 'files' | 'map';
   name: string;
   style?: React.CSSProperties;
 }
@@ -51,6 +52,9 @@ function FieldView(props: IFieldView) {
           const files = value?.fileList ?? value ?? Array.prototype;
           return files.map((file) => <Tag children={file.name} key={`attachment-${file.uid}`} />);
         }
+
+        case 'map':
+          return <MapView isReadOnly markers={value} />;
 
         default:
           return value;
